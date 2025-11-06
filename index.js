60e6f09ce9199b1742e81b6246022bc2e2115e22
UwUcodeposter <fpsgameplaye@gmail.com>
2025-11-06 18:35:50 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+// THIS IS !VIBE CODED (! means not in js)
+
+let admin;
+let feedback;
+
+window.onload = () => {
+    admin = false;
+
+    if (typeof(Storage) !== undefined) {
+        console.log('Warning twin u dont hav local storage')
+    }
+}
+
+function feedbackAlert() {
+    if (localStorage.getItem(feedback) === null) {
+        feedback = prompt('Feedback...');
+        if (admin === true) {
+            alert(feedback);
+            console.log(`Feedback ${feedback}`);
+        }
+
+        localStorage.setItem('feedback', feedback);
+    } else alert('No double feedback');
+}
+
+function roleAdmin() {
+    admin = !admin;
+    console.log(`Admin:  ${admin}`);
+}
