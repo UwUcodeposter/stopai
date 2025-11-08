a6c2bdb3de2204876a31fe94cb4896adf8ffd3c4
UwUcodeposter <fpsgameplaye@gmail.com>
2025-11-08 13:07:51 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -7,11 +7,25 @@ window.onload = () => {
     admin = false;
 
     if (typeof(Storage) !== undefined) {
-        console.log('Warning twin u dont hav local storage')
+        console.log('Warning twin u dont hav local storage');
     }
 }
 
+function UnderConstruction() {
+    alert('This part of the website is under constuction')
+}
+
 function feedbackAlert() {
+    const email = 'fpsgameplaye@gmail.com';
+    const subject = "Website Feedback - Automated Message";
+    const body = "Please provide your feedback here:\n\n";
+    const mailtoLink = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+
+    window.location.href = mailtoLink;
+}
+
+function oldFeedbackAlert() {
+    
     if (localStorage.getItem(feedback) === null && feedback === '' | feedback === undefined) {
         feedback = prompt('Feedback...');
         if (admin === true) {
